08d74b274876ca2b83d04acd79a043321adf864d
dcsv <dianacsanchez@unicauca.edu.co>
2025-06-09 17:12:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, a
 with st.sidebar:
     st.header("\U0001F4C2 Dataset predeterminado")
     try:
-        df = pd.read_csv("6 class csv.csv", delim_whitespace=True, header=None)
-        df.columns = [
-            "Temperature (K)", "Luminosity(L/Lo)", "Radius(R/Ro)",
-            "Absolute magnitude(Mv)", "Star type", "Star color", "Spectral Class"
-        ]
+        df = pd.read_csv("6 class csv.csv", sep=r"\s+")
         st.success("\u2705 Dataset cargado desde el repositorio.")
     except FileNotFoundError:
         st.error("\u274C No se encontró el archivo en el repositorio.")
