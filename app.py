245813d95b1d4b4d0012e3150f5665f6ae653653
dcsv <dianacsanchez@unicauca.edu.co>
2025-06-09 20:51:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import time
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import urllib.request
 from sklearn.base import BaseEstimator, TransformerMixin
+import re
 
 # ------------------------------------------------------------------------------
 # 1. Sidebar: cargador de archivos
